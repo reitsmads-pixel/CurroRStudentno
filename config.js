ece45450fc1d692e3e19068e6dba54795b8a1e02
reitsmads-pixel <reitsmads@gmail.com>
2025-11-06 16:00:49 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA5US45QlRkkIgNzvGY8NxOtgxhtBFrVV0",
+    authDomain: "db-test-4e542.firebaseapp.com",
+    databaseURL: "https://db-test-4e542-default-rtdb.firebaseio.com",
+    projectId: "db-test-4e542",
+    storageBucket: "db-test-4e542.firebasestorage.app",
+    messagingSenderId: "635686299111",
+    appId: "1:635686299111:web:44c438ded2deeb87fe0942"
+};
+
+// EmailJS Configuration
+const EMAILJS_PUBLIC_KEY = 'Vw5a0CwDdKhv90Z-N';
+const EMAILJS_SERVICE_ID = 'service_wu805b1';
+const EMAILJS_TEMPLATE_ID = 'template_4dhxbfk';
